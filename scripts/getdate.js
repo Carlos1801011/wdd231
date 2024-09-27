e2dac5a18b101e3b72b9bc58d457e82fceae6c9f
carlos1801011 <cha21074@byui.edu>
2024-09-27 11:37:18 -0600

    ITS OK

--- scripts/getdate.js
+++ scripts/getdate.js
@@ -1,3 +1,14 @@
+const menu = document.querySelector("#menu").addEventListener("click", () => {
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.style.display = "block";
+});
+
+const hideMenu = document.querySelector(".close-menu").addEventListener("click", () => {
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.style.display = "none";
+});
+
+
 const lastModified = document.getElementById("lastM");
 
 const LastModifiedDate = new Date(document.lastModified);
