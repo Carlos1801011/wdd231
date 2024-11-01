e475c2e83397f70785149102b397b21a1baf7d00
carlos1801011 <cha21074@byui.edu>
2024-10-31 19:10:15 -0600

    uuup

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -2,5 +2,12 @@
 import('./common.js');
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    const navButton = document.getElementById('navButton');
+    const nav = document.querySelector('nav');
 
-
+    navButton.addEventListener('click', () => {
+        nav.classList.toggle('open'); 
+        navButton.classList.toggle('open'); 
+    });
+});
